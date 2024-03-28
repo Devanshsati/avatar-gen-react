6b580dcc5673ab1df25179ee65d9895e708ced1b
Devanshsati <thedevanshsati@gmail.com>
2024-03-28 11:59:25 +0530

    link provide

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+// https://www.geeksforgeeks.org/how-to-create-an-avatar-generator-app-in-reactjs/
+// https://www.dicebear.com/guides/use-the-library-with-react/
+// https://github.com/mui/material-ui/blob/v5.15.14/docs/data/material/getting-started/templates/landing-page/components/Footer.js
+// https://mui.com/material-ui/getting-started/
+
 function App() {
 
   return (
