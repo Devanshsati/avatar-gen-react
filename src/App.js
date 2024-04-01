375215e9978da4d80f1221a0918eaf7baa9c34a8
Devanshsati <thedevanshsati@gmail.com>
2024-04-01 20:15:25 +0530

    link removes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Avatar from './components/Avatar'
 
-// https://www.geeksforgeeks.org/how-to-create-an-avatar-generator-app-in-reactjs/
-// https://www.dicebear.com/guides/use-the-library-with-react/
-// https://mui.com/material-ui/getting-started/
-
 function App() {
 
   return (
