6a3a73ab236aaa26a8da70cf629b9226857d501b
Devanshsati <thedevanshsati@gmail.com>
2024-04-01 20:28:37 +0530

    button update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,8 +45,8 @@ function Header() {
                             </Box>
                         </Box>
                         <Box sx={{display:{xs:'none',md:'flex'},gap:.5,alignItems:'center',}}>
-                            <Button color="primary" variant="text" size="small" component="a" href="/material-ui/getting-started/templates/sign-in/" target="_blank">Sign in </Button>
-                            <Button color="primary" variant="contained" size="small" component="a" href="/material-ui/getting-started/templates/sign-up/" target="_blank">Sign up </Button>
+                            <Button color="primary" variant="text" size="small" disableElevation>Sign in </Button>
+                            <Button color="primary" variant="contained" size="small" disableElevation>Sign up </Button>
                         </Box>
                         <Box sx={{ display: { sm: '', md: 'none' } }}>
                             <Button variant="text" color="primary" aria-label="menu" onClick={toggleDrawer(true)}sx={{minWidth:'30px',p:'4px'}}><MenuIcon /></Button>
